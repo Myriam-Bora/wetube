002df9f1f227f758365c05e4f0a911a601378e19
Myriam-Bora <dafnerina59@gmail.com>
2020-10-26 18:30:41 +0900

    #2.18 search controller
    input으로 입력한 값을 컨트롤러가 받아변수에 담고 다시 뷰로 보내기

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,10 @@
 export const homeController = (req,res) => res.render("home", {pageTitle:"Home"}) // render(템플릿이름 , 템플릿에 추가할 정보)
-export const searchController = (req,res) => res.render("search" , {pageTitle:"Search"})
+export const searchController = (req,res) => {
+    const {query : {term : searchingBy}} = req;     
+    //ES6 방식 - > const searchingBy = req.query.term 과 같은 말이다.
+    console.log(searchingBy)
+    res.render("search" , {pageTitle:"Search", searchingBy:searchingBy})
+}
 export const uploadController = (req,res) => res.render("upload", {pageTitle:"Upload Video"});
 export const videoDetailController = (req,res) => res.render("videoDetail", {pageTitle:"Video Detail"});
 export const editVideoController = (req,res) => res.render("editVideo", {pageTitle:"Edit Video"});
