75eb1bd99a53ae39059598dd1ff718492ddafd36
Myriam-Bora <dafnerina59@gmail.com>
2020-10-26 15:12:25 +0900

    #2.14 레이아웃(header,footer..) 만들기

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
 export const homeController = (req,res) => res.render("home")   // .render : views폴더에서 파일명이 home이고 확장자가 pug인 파일을 찾은 후 보여준다
-export const searchController = (req,res) => res.send("search!")
-export const videosController = (req,res) => res.send("videosController");
-export const uploadController = (req,res) => res.send("uploadController");
-export const videoDetailController = (req,res) => res.send("videoDetailController");
-export const editVideoController = (req,res) => res.send("editVideoController");
-export const deleteVideoController = (req,res) => res.send("deleteVideoController");
+export const searchController = (req,res) => res.render("search")
+export const videosController = (req,res) => res.render("videos");
+export const uploadController = (req,res) => res.render("upload");
+export const videoDetailController = (req,res) => res.render("videoDetail");
+export const editVideoController = (req,res) => res.render("editVideo");
+export const deleteVideoController = (req,res) => res.render("deleteVideo");
