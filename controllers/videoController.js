8cedaf13bf4af386715d0a3360c10848c5e3c780
Myriam-Bora <dafnerina59@gmail.com>
2020-10-26 17:58:59 +0900

    #2.17 템플릿 마다 다른 변수 보내기
    - videoController 삭제

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,6 @@
-export const homeController = (req,res) => res.render("home")   // .render : views폴더에서 파일명이 home이고 확장자가 pug인 파일을 찾은 후 보여준다
-export const searchController = (req,res) => res.render("search")
-export const videosController = (req,res) => res.render("videos");
-export const uploadController = (req,res) => res.render("upload");
-export const videoDetailController = (req,res) => res.render("videoDetail");
-export const editVideoController = (req,res) => res.render("editVideo");
-export const deleteVideoController = (req,res) => res.render("deleteVideo");
+export const homeController = (req,res) => res.render("home", {pageTitle:"Home"}) // render(템플릿이름 , 템플릿에 추가할 정보)
+export const searchController = (req,res) => res.render("search" , {pageTitle:"Search"})
+export const uploadController = (req,res) => res.render("upload", {pageTitle:"Upload Video"});
+export const videoDetailController = (req,res) => res.render("videoDetail", {pageTitle:"Video Detail"});
+export const editVideoController = (req,res) => res.render("editVideo", {pageTitle:"Edit Video"});
+export const deleteVideoController = (req,res) => res.render("deleteVideo", {pageTitle:"Delete Video"});
