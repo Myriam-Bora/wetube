f039827fd96882b0633e3c293af162db60d2672b
Myriam-Bora <dafnerina59@gmail.com>
2020-10-29 00:46:47 +0900

    #3.4 comment model 만들기
    - 코멘트 모델 만들기
    - 방법1 : 코멘트에 해당 비디오 id를
    - 방법2 : 비디오에 코멘트 배열 아이디들을 담기

--- models/Video.js
+++ models/Video.js
@@ -16,8 +16,13 @@ const VideoSchema = new mongoose.Schema({
     },
     createdAt:{
         type:Date,
-        default:Date.now             //현재시각
-    }
+        default:Date.now
+    },
+    // []를 쓰는 이유 : 비디오 하나에 코멘트는 여러개 이기 때문
+    comments:[{
+        type:mongoose.Schema.Types.ObjectId,   //다른 스키마의 타입을 사용
+        ref:"Comment"                        //해당 되는 스키마의 아이디
+    }]
 });
 
 //위의 스키마를 이용해 Model 만들기
