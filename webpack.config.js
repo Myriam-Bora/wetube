b1dd9800364f07a1f71a762e30b6f74dffecf95b
Myriam-Bora <dafnerina59@gmail.com>
2020-11-02 20:37:07 +0900

    #4.1~2 webpack2
    - enviroment(환경)를 webpack이 넘겨받지 못해서 수동으로 env를 넘겨받도록 한다
    - 윈도우환경에서 진행할 때 'WEBPACK_ENV'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는
    배치 파일이 아닙니다. 에러가 뜸
    - npm install cross-env  설치 이후
    - package.json 수정
    -npm install extract-text-webpack-plugin@next  : 베타가 설치된다
    - @: 어느버전을 쓸건지 명시
    
    -Autoprefixer 설치
    - 반드시 버전을 낮춰서 설치하기
    npm install postcss-loader@3.0.0
    npm install css-loader@1.0.1
    npm install sass-loader@7.1.0
    npm install node-sass@4.14.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 // path는 노드에서 기본적으로 있는 패키지. 절대경로를 나타내어준다 users/nicolas/documents/wetube/asstes..
 
+//웹팩은 모던 자바스크립트이기 때문에 import 사용이 불가! -> require 사용
 const path = require("path");
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
 
 //entry : 파일이 어디서 왔는가
 //__dirname : 현재의 프로젝트 디렉토리 이름. 어디서든 접근가능한 노트의 전역변수
@@ -10,10 +15,45 @@ const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
     entry : ENTRY_FILE,
+    mode:MODE,
+    module:{
+        rules:[
+            {
+                test: /\.(scss)$/,              //조건 : scss을 만나게 되면, 아래와 같은 플러그인을 사용한다
+                use:ExtractCSS.extract([     //내부에 또 다른 플러그인을 사용하고 있다, 아래 단계가 끝나면 css를 텍스트로 추출
+                    {
+                        loader:"css-loader" //웹팩이 css를 이해하게 된다
+                    },
+                    {
+                        loader:"postcss-loader", //아래에서 받은 것들을  css로 변환
+                        options:{
+                            plugin(){
+                                return [autoprefixer({browsers:"cover 99.5%"})] 
+                                 //옵션 설정 모든 브라우저에서 99.5% 호환되게 만들어준다
+                            }
+                        }
+                    },
+                    {
+                        loader:"sass-loader"   //sass or scss를 받는다
+                    }
+                ])
+            }
+        ]
+
+    },
     output : {
         path : OUTPUT_DIR,
-        filename : "[name].[format]"
-    }
+        filename : "[name].js"
+    },
+    //플러그인 설치
+    plugins:[new ExtractCSS("styles.css")]
+
 };
 
 module.exports = config;
+
+//module:rules : 웹팩에게 규칙을 알려준다
+// 1. scss를 찾아서 css로 바꿔준다
+//2. css를 텍스트로 추출
+//3. 분리된 하나의 파일로 만든다
+//단, 웹팩은 순서진행이 아래에서 위로 진행되기 때문에 순서를 반대로 작성한다
