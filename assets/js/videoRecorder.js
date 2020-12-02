3a6cb366c24cae138c40d7685c0c23eb544053d8
Myriam-Bora <dafnerina59@gmail.com>
2020-12-02 20:18:07 +0900

    #9.1 비디오 녹화(미해결)
    - mediaRecorder : getUserMedia로 얻어진 stream을 저장
    - videoRecorder출력하면 나오는 dataavailable : 버튼을 클릭하면 여러개의 addEventListener을 넣을 수 있게 해준다

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,19 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = event => {
+  console.log(event);
+};
+
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({   // getUserMedia : 녹화 권한주기
       audio: true,
@@ -11,14 +23,19 @@ const startRecording = async () => {
     videoPreview.srcObject = stream;  // srcObject : 비디오는 파일이 아닌 객체이기 때문에 사용
     videoPreview.muted = true;  //녹화하면서 내 목소리 들리지 않게 하기
     videoPreview.play(); 
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "☹️ Cant record";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
   recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
